1c0f6678eb473a1b175e1dd05fe5e48913d0e95a
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-01 11:35:28 -0500

    Add header

---
+++ src/components/Header.svelte
@@ -0,0 +1,3 @@
+<header class="bg-impervia-blue flex max-h-20 min-h-16 items-center px-32 text-white">
+	<h2 class="text-3xl font-extrabold">IMPERVIA</h2>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+</script>
+
 <svelte:head>
 	<title>Impervia | Protege tu casa de la humedad</title>
 </svelte:head>
+
+<Header />
