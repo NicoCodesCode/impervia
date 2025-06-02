1b394fbe13fbce51eb9083e1dc92cf5f34dab583
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-02 14:43:04 -0500

    Add hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Header from '../components/Header.svelte';
-	import Hero from '../components/Hero.svelte';
 </script>
 
 <svelte:head>
@@ -8,4 +7,15 @@
 </svelte:head>
 
 <Header />
-<Hero />
+<section
+	class="bg-impervia-darker-blue flex min-h-[calc(100vh-4rem)] items-center justify-evenly px-32"
+>
+	<div class="font-poppins max-w-xl text-white">
+		<h1 class="mb-4 text-5xl/tight font-extrabold">
+			Protege Tu <span class="text-impervia-yellow">Casa</span> De La
+			<span class="text-impervia-blue">Humedad</span>
+		</h1>
+		<p>Impervia mantiene la humedad fuera, para que tu hogar permanezca seco, limpio y fuerte.</p>
+	</div>
+	<img class="h-96" src="impervia-logo.png" alt="Impervia logo" />
+</section>
