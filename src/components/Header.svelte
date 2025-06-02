9103da4b160a737f3fd453f920d402863e75ea8e
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-02 15:18:04 -0500

    Make page responsive

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,3 @@
 <header class="bg-impervia-blue flex h-16 items-center px-10 text-white sm:px-16 lg:px-32">
-	<h2 class="text-3xl font-extrabold">IMPERVIA</h2>
+	<h2 class="text-2xl font-extrabold sm:text-3xl">IMPERVIA</h2>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,15 @@
 </svelte:head>
 
 <Header />
-<!-- <section
-	class="bg-impervia-darker-blue flex min-h-[calc(100vh-4rem)] items-center justify-evenly px-32"
+<section
+	class="bg-impervia-darker-blue flex min-h-[calc(100vh-4rem)] flex-wrap justify-evenly px-10 pt-16 text-center sm:items-center sm:px-16 md:text-left lg:px-32 lg:pt-0"
 >
-	<div class="font-poppins max-w-xl text-white">
-		<h1 class="mb-4 text-5xl/tight font-extrabold">
+	<div class="font-poppins h-min max-w-sm text-white sm:max-w-xl">
+		<h1 class="mb-4 text-3xl/snug font-extrabold sm:text-5xl/tight">
 			Protege Tu <span class="text-impervia-yellow">Casa</span> De La
 			<span class="text-impervia-blue">Humedad</span>
 		</h1>
 		<p>Impervia mantiene la humedad fuera, para que tu hogar permanezca seco, limpio y fuerte.</p>
 	</div>
-	<img class="h-96" src="impervia-logo.png" alt="Impervia logo" />
-</section> -->
+	<img class="h-72 sm:h-96" src="impervia-logo.png" alt="Impervia logo" />
+</section>
