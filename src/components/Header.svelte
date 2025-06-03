3f6f69326446ba8c772fd19a27acf7ccd902ad4e
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-03 16:25:29 -0500

    Minor header changes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,14 @@
 <header
-	class="bg-impervia-blue fixed flex h-16 w-screen items-center px-10 text-white sm:px-16 lg:px-32"
+	class="bg-impervia-blue flex min-h-16 w-screen flex-wrap items-center justify-between px-10 py-4 text-white sm:fixed sm:px-16 lg:px-32"
 >
 	<h2 class="text-2xl font-extrabold sm:text-3xl">IMPERVIA</h2>
+	<!-- TODO: Finish navbar -->
+	<!-- <nav class="text-md flex flex-wrap justify-center gap-4 text-center font-semibold">
+		<a href="#impervia-description">
+			<p>¿Qué es Impervia?</p>
+		</a>
+		<a href="#instructions">
+			<p>Instrucciones de uso</p>
+		</a>
+	</nav> -->
 </header>
