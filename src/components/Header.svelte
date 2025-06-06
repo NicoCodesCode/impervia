3f50b5132a917942858eef9b80d75dd760a5d1fc
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-06 15:24:04 -0500

    Add comments

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,11 +3,13 @@
 
 	let top: NavbarTop = $state('-top-full');
 
+	// Toggle top position of navbar on small screens
 	function showNavbar() {
 		top = top === '-top-full' ? 'top-[4rem]' : '-top-full';
 	}
 </script>
 
+<!-- Phone navbar -->
 <nav
 	class="text-md bg-impervia-blue fixed {top} flex w-full flex-col items-center gap-4 pb-8 font-semibold text-white transition-all duration-300 sm:hidden"
 >
@@ -18,10 +20,13 @@
 		<ul>Instrucciones de uso</ul>
 	</a>
 </nav>
+
 <header
 	class="bg-impervia-blue fixed flex min-h-16 w-screen flex-wrap items-center justify-between px-10 py-4 text-white sm:px-16 lg:px-32"
 >
 	<h2 class="text-2xl font-extrabold sm:text-3xl">IMPERVIA</h2>
+
+	<!-- Desktop navbar -->
 	<nav class="text-md hidden flex-wrap justify-center gap-4 text-center font-semibold sm:flex">
 		<a href="#impervia-description">
 			<ul>¿Qué es Impervia?</ul>
@@ -30,6 +35,8 @@
 			<ul>Instrucciones de uso</ul>
 		</a>
 	</nav>
+
+	<!-- Hamburger button -->
 	<button onclick={showNavbar} class="hover:cursor-pointer sm:hidden">
 		<img class="h-7 invert" src="/icons/hamburger.png" alt="Navbar menu icon" />
 	</button>
