46ca69e6894cc6b530b6ac4904cf957aba8d0b84
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-06 09:15:51 -0500

    Fix style

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <header
-	class="bg-impervia-blue flex min-h-16 w-screen flex-wrap items-center justify-between px-10 py-4 text-white sm:fixed sm:px-16 lg:px-32"
+	class="bg-impervia-blue fixed flex min-h-16 w-screen flex-wrap items-center justify-between px-10 py-4 text-white sm:px-16 lg:px-32"
 >
 	<h2 class="text-2xl font-extrabold sm:text-3xl">IMPERVIA</h2>
 	<!-- TODO: Finish navbar -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,16 +14,16 @@
 
 <!-- Hero section -->
 <section
-	class="bg-impervia-darker-blue flex min-h-[calc(100vh-4rem)] flex-wrap justify-evenly px-10 pt-16 sm:items-center sm:px-16 lg:px-32 lg:pt-0"
+	class="bg-impervia-darker-blue flex min-h-[calc(100vh-4rem)] flex-wrap justify-evenly px-10 py-24 sm:items-center sm:px-16 lg:px-32"
 >
 	<div class="font-poppins h-min max-w-sm text-white sm:max-w-xl">
-		<h1 class="mb-4 text-3xl/snug font-extrabold sm:text-4xl/tight md:text-5xl/tight">
+		<h1 class="mb-4 text-3xl/snug font-extrabold sm:text-4xl/tight lg:text-5xl/tight">
 			Protege Tu <span class="text-impervia-yellow">Casa</span> De La
 			<span class="text-impervia-blue">Humedad</span>
 		</h1>
 		<p>Impervia mantiene la humedad fuera, para que tu hogar permanezca seco, limpio y fuerte.</p>
 	</div>
-	<img class="h-72 sm:h-96" src="impervia-logo.png" alt="Impervia logo" />
+	<img class="h-72 sm:h-80" src="impervia-logo.png" alt="Impervia logo" />
 </section>
 
 <!-- Humidity threats section -->
