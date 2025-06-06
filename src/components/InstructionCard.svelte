abc2cb3f0dfb94d2e09781a21be9b22369c5d8ec
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-06 08:51:39 -0500

    Change gray color

--- src/components/InstructionCard.svelte
+++ src/components/InstructionCard.svelte
@@ -4,5 +4,5 @@
 
 <div class="bg-impervia-dark-blue mb-8 max-w-3xl rounded-xl px-10 py-6 shadow-lg/40">
 	<h3 class="mb-2 text-lg font-semibold sm:text-2xl">{title}</h3>
-	<p class="text-gray-300">{text}</p>
+	<p class="text-gray-200">{text}</p>
 </div>
