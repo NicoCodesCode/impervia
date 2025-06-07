cd2219359e7af25edfbcce00d41db5c75f577ef9
NicoCodesCode <nicolasrmz08@gmail.com>
2025-06-07 10:06:07 -0500

    Add mission and vision

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -11,7 +11,7 @@
 
 <!-- Phone navbar -->
 <nav
-	class="text-md bg-impervia-blue fixed {top} flex w-full flex-col items-center gap-4 pt-3 pb-9 font-semibold text-white transition-all duration-300 lg:hidden"
+	class="bg-impervia-blue text-md fixed {top} flex w-full flex-col items-center gap-4 pt-3 pb-9 font-semibold text-white transition-all duration-300 lg:hidden"
 >
 	<a
 		class="hover:underline active:text-gray-200"
@@ -26,18 +26,21 @@
 	<a class="hover:underline active:text-gray-200" onclick={toggleNavbar} href="/calculator">
 		<ul>Calculadora de cubetas</ul>
 	</a>
+	<a class="hover:underline active:text-gray-200" onclick={toggleNavbar} href="/about">
+		<ul>Sobre la empresa</ul>
+	</a>
 </nav>
 
 <!-- Header -->
 <header
-	class="bg-impervia-blue fixed flex min-h-16 w-screen flex-wrap items-center justify-between px-10 py-4 text-white sm:px-16 lg:px-32"
+	class="bg-impervia-blue fixed flex min-h-16 w-screen flex-wrap items-center justify-between px-10 py-4 text-white sm:px-16 xl:px-28"
 >
 	<a href="/">
 		<h2 class="text-2xl font-extrabold sm:text-3xl">IMPERVIA</h2>
 	</a>
 
 	<!-- Desktop navbar -->
-	<nav class="text-md hidden flex-wrap justify-center gap-4 text-center font-semibold lg:flex">
+	<nav class="hidden flex-wrap justify-center gap-4 text-center text-sm font-semibold lg:flex">
 		<a class="hover:underline active:text-gray-200" href="/#impervia-description">
 			<ul>¿Qué es Impervia?</ul>
 		</a>
@@ -46,6 +49,9 @@
 		</a>
 		<a class="hover:underline active:text-gray-200" href="/calculator">
 			<ul>Calculadora de cubetas</ul>
+		</a>
+		<a class="hover:underline active:text-gray-200" href="/about">
+			<ul>Sobre la empresa</ul>
 		</a>
 	</nav>
 
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,36 @@
+<section
+	class="bg-impervia-dark-blue relative -z-20 flex min-h-[calc(100vh-4rem)] flex-wrap items-center justify-center gap-8 px-10 py-24 sm:px-16 lg:px-32"
+>
+	<img
+		class="absolute top-1/2 left-1/2 -z-10 h-60 -translate-x-1/2 -translate-y-1/3 opacity-20 sm:h-80"
+		src="impervia-logo.png"
+		alt="Impervia logo"
+	/>
+	<h1 class="text-3xl/snug font-extrabold text-white sm:text-4xl/tight lg:text-5xl/tight">
+		Sobre La <span class="text-impervia-yellow">Empresa</span>
+	</h1>
+	<div class="flex w-full flex-wrap items-center justify-center gap-8 lg:justify-evenly">
+		<div
+			class="bg-impervia-darker-blue/60 min-h-48 max-w-sm rounded-xl p-10 text-center shadow-lg/40"
+		>
+			<h2 class="text-impervia-yellow mb-4 text-2xl/snug font-extrabold sm:text-3xl/tight">
+				MISIÓN
+			</h2>
+			<p class="text-white">
+				Ser la marca líder en impermeabilizantes, reconocida por su calidad, innovación y nuestro
+				compromiso con el cuidado del medio ambiente
+			</p>
+		</div>
+		<div
+			class="bg-impervia-darker-blue/60 min-h-48 max-w-sm rounded-xl p-10 text-center shadow-lg/40"
+		>
+			<h2 class="text-impervia-yellow mb-4 text-2xl/snug font-extrabold sm:text-3xl/tight">
+				VISIÓN
+			</h2>
+			<p class="text-white">
+				Brindar productos de alta calidad a precios accesibles para proteger los hogares de nuestros
+				clientes con compromiso, confianza y eficacia
+			</p>
+		</div>
+	</div>
+</section>
